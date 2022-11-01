0d74d86ed74365098e44ff54c6c6e56a67673f1c
Misael <misaelcudek@gmail.com>
2022-11-01 18:28:25 -0300

    Add files via upload

---
+++ Resueltos_LenguajeC/resueltosSockets/serverProcesaArchivoyDevuelve/cliente_txarchivo.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<stdlib.h>
+#include <unistd.h>
+#include <stdio_ext.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<errno.h>
+
+#define archmax 2048
+
+
+struct archivo {
+char nombre [256];
+unsigned long int tam;
+char bytesDelArchivo[archmax];
+};
+
+union vinculo
+{
+    char buffer[sizeof(struct archivo)];
+    struct archivo  dato;
+};
+
+
+int main(int argc , char *argv[])
+{
+
+    long int tama=0;//Puede ser -1 si es error
+	int leido=0;
+    int socket_desc;
+
+    int fdw=0;
+    struct sockaddr_in server;
+
+    union vinculo convertir;
+
+
+    int byterx=0;
+
+
+//abrir el archivo
+//cargarlo en ram
+//ver que no supere tam archmax
+
+    //if(argc==2) checkear que este ok
+
+	int fd = open(argv[1], O_RDONLY );
+	if (fd ==-1)
+	{
+		// print which type of error have in a code
+		printf("Error Number %d \n", errno);
+
+		// print program detail "Success or failure"
+		perror("Program");
+	}else
+    {
+	tama=lseek(fd,0,SEEK_END);//pongo al final
+    printf("El tam del archivo es %ld\n",tama);
+    }
+
+
+	if(tama>0 && tama <= 2048)
+	{
+
+
+	lseek(fd,0,SEEK_SET);//pongo al inicio
+	leido=read(fd,convertir.dato.bytesDelArchivo,tama);
+    close(fd);
+    convertir.dato.tam=tama;
+    strcpy(convertir.dato.nombre,argv[1]);
+
+    }else
+    {
+        printf("Tamaño no valido \n");
+        exit(1);
+    }
+
+	if(leido==tama)
+	{
+         //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Error");
+    }
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+
+
+    //Connect to remote server
+    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("Error en connect");
+        return 1;
+    }
+
+    puts("Connectado\n");
+
+    write(socket_desc , convertir.buffer , sizeof(struct archivo));
+
+
+ byterx=recv(socket_desc, convertir.buffer , sizeof(struct archivo) , 0);
+
+strcat(convertir.dato.nombre,"M");
+
+ fdw = open(convertir.dato.nombre, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH );
+
+    if(fdw==-1)
+        {
+            printf("Error\n");
+            return(1);
+        }
+
+    write(fdw, (convertir.buffer+256+sizeof(unsigned long int)) ,convertir.dato.tam );
+
+    close(fdw);
+    close(socket_desc);
+
+    }
+
+
+
+
+
+
+    return 0;
+}
---
+++ Resueltos_LenguajeC/resueltosSockets/serverProcesaArchivoyDevuelve/server_rxarchivo.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define archmax 2048
+
+
+struct archivo {
+char nombre [256];
+unsigned long int tam;
+char bytesDelArchivo[archmax];
+};
+
+union vinculo
+{
+    char buffer[sizeof(struct archivo)];
+    struct archivo  dato;
+};
+
+
+int main(int argc , char *argv[])
+{
+    int socket_desc , new_socket , c;
+    struct sockaddr_in server , client;
+
+    union vinculo convertir;
+
+
+    int byterx=0;
+    char * p=NULL;
+
+
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("No se puedo crear el socket");
+    }
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+
+
+    //Ojo esto arranca el puerto
+    int yes=1;
+    if (setsockopt(socket_desc,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1)
+    {
+    perror("setsockopt");
+    exit(1);
+    }
+
+
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("bind fallo");
+        return 1;
+    }
+    puts("bind OK");
+
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Espero a que un cliente se me conecte en el accept");
+    c = sizeof(struct sockaddr_in);
+    new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (new_socket<0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+
+
+
+    puts("Cliente conectado");
+    printf("IP cliente: %s\t Puerto cliente: %d\n",inet_ntoa(client.sin_addr),client.sin_port);
+    printf("Recibiendo estructuras del cliente\n");
+
+
+
+
+    byterx=recv(new_socket, convertir.buffer , sizeof(struct archivo) , 0);
+
+    //
+    p=convertir.buffer+256+sizeof(unsigned long int);
+
+    for(int i=0;i<convertir.dato.tam;i++)
+    {
+    if( *(p+i) >= 'a' && *(p+i) <= 'z')
+        {
+        *(p+i)=*(p+i)- ( 'a'-'A');
+        }
+    }
+
+    write(new_socket , convertir.buffer , sizeof(struct archivo));
+
+    /*write(fd, (convertir.buffer+256+sizeof(unsigned long int)) ,convertir.dato.tam );*/
+//
+
+
+
+
+
+/*
+    if(byterx==sizeof(struct persona))
+    {
+        printf("Estructura correcta\n");
+    }
+*/
+
+    //mostrar que el archivo llego
+
+
+    close(new_socket);
+    close(socket_desc);
+
+
+    return 0;
+}
